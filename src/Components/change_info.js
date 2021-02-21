ca234ae367e6e5c7b6faf4153173832f388630a0
aquarius6666 <aqaurius6666@gmail.com>
2021-02-21 22:48:34 +0700

    a

--- src/Components/change_info.js
+++ src/Components/change_info.js
@@ -1,8 +1,7 @@
 
-import { useContext, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { api_get_student, api_update_student } from "../API/action"
-import { ChangePassword } from "./change_password"
-import userContext from "./userContext"
+
 
 export const ChangeInfo = (props) => {
     const [form, setForm] = useState(undefined)
@@ -11,28 +10,31 @@ export const ChangeInfo = (props) => {
     }, [])
     const onSubmitForm = (e) => {
         e.preventDefault()
-        api_update_student(form, (msg) =>console.log(msg))
-
+        api_update_student(form, (msg) => console.log(msg))
     }
     if (form) {
         return (
             <>
-            <label>Info: </label>
-            <form onSubmit={onSubmitForm}>
-                <div>
-                    <label>Name: </label>
-                    <input value={form.name ? form.name : ""} onChange={(e) => setForm({ ...form, name: e.target.value })}></input>
-                </div>
+                <label>Info: </label>
+                <form onSubmit={onSubmitForm}>
+                    <div>
+                        <label>Name: </label>
+                        <input value={form.name ? form.name : ""} onChange={(e) => setForm({ ...form, name: e.target.value })}></input>
+                    </div>
 
-                <div>
-                    <label>Date of birth: </label>
-                    <input value={form.dob ? form.dob : ""} placeholder="dd/mm/yyyy"onChange={(e) => setForm({...form, dob: e.target.value})}></input>
-                </div>
-                <div>
-                    <label>House ID: </label>
-                    <input value={form.hid ? form.hid : ""} onChange={(e) => setForm({ ...form, hid: e.target.value })}></input>
-                </div>
-
+                    <div>
+                        <label>Date of birth: </label>
+                        <input value={form.dob ? form.dob : ""} placeholder="dd/mm/yyyy" onChange={(e) => setForm({ ...form, dob: e.target.value })}></input>
+                    </div>
+                    <div>
+                        <label>House: </label>
+                        <select name="hid" >
+                            <option value="1000">Gryffindor</option>
+                            <option value="1001">Slytherin</option>
+                            <option value="1002">Ravenclaw</option>
+                            <option value="1003">Hufflepuff</option>
+                        </select>
+                    </div>
                     <button>Update</button>
                 </form>
             </>
