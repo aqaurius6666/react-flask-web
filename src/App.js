ccd5284ea617b91129fc1eab442cfed0a5982643
aquarius6666 <aqaurius6666@gmail.com>
2021-03-23 22:45:31 +0700

    fix import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+import { Bar } from 'react-chartjs-2';
+import { useEffect } from 'react/cjs/react.production.min';
 import './App.css';
-import BarChart from "./Components/chartComponent";
+import BarChart, { fetchData } from "./Components/chartComponent";
 
 function App() {
   return (
     <div className="App">
 
-      {BarChart}
+      <BarChart />
     </div>
   );
 }
--- src/Components/chartComponent.js
+++ src/Components/chartComponent.js
@@ -1,7 +1,5 @@
-import React, {Component} from 'react';
+import React, {useEffect, useRef, useState}  from 'react';
 import Chartjs from 'chart.js/dist/Chart';
-import {Bar} from 'react-chartjs-2';
-import {useEffect, useRef, useState} from "react/cjs/react.production.min";
 
 const chartConfig ={
     type: 'bar',
@@ -18,19 +16,28 @@ const chartConfig ={
     }
 }
 
-const fetchData = () => {
-    const url = `https://it-must-be-ok.herokuapp.com/`
+export const fetchData = (course) => {
+    const url = `http://127.0.0.1:5000/api/graph?course=${course}`
+    const requestOptions = {
+        method : 'GET'
+    }
+    return fetch(url, requestOptions).then(data => {
+        
+        return data.json()
+    }
+    )
 }
 
 
-function BarChart() {
-    const chartContainer = useRef(null);
-    const [chartInstance, setChartInstance] = useState(null);
+const BarChart = () => {
+    const chartContainer = useRef(null)
+    const [chartInstance, setChartInstance] = useState(null)
 
     useEffect(() => {
+
         if (chartContainer && chartContainer.current) {
-            const newChartInstance = new Chartjs(chartContainer.current, chartConfig);
-            setChartInstance(newChartInstance);
+            //const newChartInstance = new Chartjs(chartContainer.current, chartConfig);
+            //setChartInstance(newChartInstance);
         }
     }, [chartContainer]);
 
